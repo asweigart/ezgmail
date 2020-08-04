b8b9726b22f064033191cb05eb8a9e8113967678
Al Sweigart <asweigart@gmail.com>
2020-08-03 22:01:20 -0700

    Switching to calendar versioning.

--- src/ezgmail/__init__.py
+++ src/ezgmail/__init__.py
@@ -4,7 +4,7 @@
 # Note: Unless you know what you're doing, also use the default 'me' value for userId parameters in this module.
 
 
-__version__ = "0.0.8"
+__version__ = "2020.8.3"
 
 """
 NOTES FOR DEVELOPERS AND CONTRIBUTORS:
