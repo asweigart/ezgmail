6f9ce651d0046b43fd898bff9349baa3c6752d18
Al Sweigart <asweigart@gmail.com>
2021-08-05 10:11:47 -0700

    Bumping version to 2021.08.05 after running the unit tests.

--- src/ezgmail/__init__.py
+++ src/ezgmail/__init__.py
@@ -4,7 +4,7 @@
 # Note: Unless you know what you're doing, also use the default 'me' value for userId parameters in this module.
 
 
-__version__ = "2021.02.04"
+__version__ = "2021.08.05"
 
 """
 NOTES FOR DEVELOPERS AND CONTRIBUTORS:
