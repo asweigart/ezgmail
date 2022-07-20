56f74a846855697dd5dfb1d37c8715302919cc02
Al Sweigart <asweigart@gmail.com>
2022-07-20 14:09:48 -0500

    Removing the email regex because it's susceptible to DoS (see https://owasp.org/www-community/attacks/Regular_expression_Denial_of_Service_-_ReDoS ) and it isn't used by the module anyway.

--- src/ezgmail/__init__.py
+++ src/ezgmail/__init__.py
@@ -45,9 +45,6 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-# Copied from https://emailregex.com/:
-EMAIL_ADDRESS_REGEX = re.compile(r'''(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])''')
-
 # SCOPES = 'https://www.googleapis.com/auth/gmail.readonly' # read-only mode
 SCOPES = "https://mail.google.com/"  # read-write mode
 SERVICE_GMAIL = None
